455e2c3d1d8592e94ab117e3965d4fd80e4e7b60
Jason Cihelka <jcihelka@isotechnics.com>
2023-03-10 14:46:09 -0800

    feat: start script - more logging

--- start.sh
+++ start.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 echo "| starting apps"
+echo "| please wait - this will take a few seconds"
 
 DELTA_ENV=delta.env
 source $DELTA_ENV
 
-nohup ./delta/delta daemon --mode=standalone >/dev/null &
+nohup ./delta/delta daemon --mode=standalone >/dev/null 2>&1 &
 sleep 20
-nohup ./delta-dm/delta-dm daemon >/dev/null &
+nohup ./delta-dm/delta-dm daemon >/dev/null 2>&1 &
 sleep 5
-cd ./delta-nextjs-client && nohup npm run start >/dev/null &
+cd ./delta-nextjs-client && nohup npm run start >/dev/null 2>&1 &
+
+echo "| apps started"

792f2e748632a6bf1e6f19556de289759417885f
Jason Cihelka <jason.cihelka@protocol.ai>
2023-06-02 10:02:09 -0700

    Update update.sh

--- update.sh
+++ update.sh
@@ -8,7 +8,7 @@ source $DELTA_ENV
 echo "| pulling latest changes"
 (cd delta && git pull && make build >/dev/null 2>&1)
 (cd delta-dm && git pull && make build >/dev/null 2>&1)
-(cd delta-nextjs-client && git pull && npm install && npm run build >/dev/null 2>&1)
+(cd delta-ui && git pull && npm install && npm run build >/dev/null 2>&1)
 
 
 source ./stop.sh

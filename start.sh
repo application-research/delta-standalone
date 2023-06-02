7b56ff3872107ca5fa118404cd0e23f76cd346e5
Jason Cihelka <jason.cihelka@protocol.ai>
2023-06-02 10:01:55 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -9,6 +9,6 @@ nohup ./delta/delta daemon --mode=standalone >/dev/null 2>&1 &
 sleep 20
 nohup ./delta-dm/delta-dm daemon >/dev/null 2>&1 &
 sleep 5
-cd ./delta-nextjs-client && nohup npm run start >/dev/null 2>&1 &
+cd ./delta-ui && nohup npm run start >/dev/null 2>&1 &
 
 echo "| apps started"
